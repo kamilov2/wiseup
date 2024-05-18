204a5c5b4ecd9e0d0207637003b6df0c5e8abb13
Jasurbek <jasurkamilov7279@gmail.com>
2024-05-18 21:37:31 +0500

    .

--- WiseUp/settings.py
+++ WiseUp/settings.py
@@ -120,17 +120,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles') ,  
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'staticfiles') ,  
+# ]
 
-
+STATIC_URL = '/wiseup/static/'
+STATIC_ROOT = '/home/careconn/wiseup/static/'
+MEDIA_URL = '/wiseup/media/'
+MEDIA_ROOT = '/home/careconn/wiseup/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
