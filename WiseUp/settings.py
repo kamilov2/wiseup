4ec6caa81f11f07823f2cf0cd21c5abfb8564ced
Jasurbek <jasurkamilov7279@gmail.com>
2024-05-18 21:41:06 +0500

    .

--- WiseUp/settings.py
+++ WiseUp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-qc8g%5h^kxxcnv#=!x(@oknage1zw5nt0-r_=12eeyo4d1bihr"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
